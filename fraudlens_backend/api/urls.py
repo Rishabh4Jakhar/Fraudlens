893ed99391db1c3194afee2a7ebe7494193db090
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2025-05-13 15:08:50 +0530

    Added url paths

--- fraudlens_backend/api/urls.py
+++ fraudlens_backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import check_website_trust, detect_scam_email
+from .views import check_website_trust, detect_scam_email, signup, login
 
 urlpatterns = [
     path('check-website/', check_website_trust),
     path('detect-scam-email/', detect_scam_email),
+    path('signup/', signup),
+    path('login/', login),    
 ]
